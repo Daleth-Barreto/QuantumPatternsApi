27ffdc35ea543e7024ce37b84797d2d86013ad8a
Alan Daleth Hernandez Barreto <98235733+Daleth-Barreto@users.noreply.github.com>
2025-04-16 16:33:41 -0600

    Fix: CORS configuration to allow requests from specific frontend domain

--- app/main.py
+++ app/main.py
@@ -2,22 +2,21 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import patterns
 
-# Crear la aplicación FastAPI
 app = FastAPI(title="Quantum Pattern API")
 
 # Configurar CORS
 origins = [
     "http://localhost",  # Permite solicitudes desde localhost
     "http://127.0.0.1",  # Permite solicitudes desde localhost usando 127.0.0.1
-    "*",  # Permite solicitudes desde cualquier origen (esto puedes restringirlo más)
+    "https://daleth-barreto.github.io/QuantumPatternsApi/",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Lista de orígenes permitidos
     allow_credentials=True,
-    allow_methods=["*"],  # Permitir cualquier método (GET, POST, etc.)
-    allow_headers=["*"],  # Permitir cualquier cabecera
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Incluir las rutas de patrones
