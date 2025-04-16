d4cf011bfc81bdf97bc4902661177d3623c4d66d
Hernandez Barreto Alan Daleth <alandaleth.hb@gmail.com>
2025-04-16 15:54:38 -0600

    feat: deploy frontend to gh-pages

--- app/models/request_models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class PatternRequest(BaseModel):
-    width: int = 100
-    height: int = 100
-    pattern_type: str = "simple"  # simple, circular, waves, checkerboard, other, word
-    word: str | None = None
